57475a98f0d454aed5c84e4bebc27efec7f27f06
Ben Domschot <Bdomschot@gmail.com>
2021-03-10 11:39:56 -0800

    Fixed css and added starting strings for password creation.

---
+++ script.js
@@ -0,0 +1,29 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+const lowerCaseOptions = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const numberOptions = ["0","1","2","3","4","5","6","7","8","9"];
+const specialCharacterOptions = ["!","#","$","%","&","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
+
+
+
+
+
+
+
+
+function generatePassword() {
+    var length = userInputLength();
+    
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
