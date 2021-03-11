bd5eac0d34b4953a930de802904e8260e62dd5ee
Ben Domschot <Bdomschot@gmail.com>
2021-03-10 21:28:01 -0800

    Added choices selections, password generation after choices, fixed errors

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+const upperCaseOptions = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 const lowerCaseOptions = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 const numberOptions = ["0","1","2","3","4","5","6","7","8","9"];
 const specialCharacterOptions = ["!","#","$","%","&","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
@@ -8,34 +9,65 @@ function userInputLength() {
   let length;
   let lengthValidate = false;
 
-  while ( lengthValidate = false){
+  while ( lengthValidate === false){
     var lengthInput = prompt("Please enter a password length between 8 and 128 characters.");
+      
+    length = parseInt(lengthInput);
 
-    if ( lengthInput === null ) return;  
-    
-    length = parseInt ( lengthInput );
-
-    if ( length.isInteger() && (length >= 8 && length <= 128)) {
+    if ( Number.isInteger(length) && (length >= 8 && length <= 128)) {
       lengthValidate = true;
-      return;
-    }
+      continue
+       }
     else {
       alert ( "Invalid entry please try again.")
-      return;
-    }
+      }
   }
 
   return length;
 }
 
+function userChoices() {
+  var choices = [];
 
+  while( choices.length === 0 ){
+    if( confirm("Do you want to use upper case letters?")) {
+      choices = choices.concat(upperCaseOptions)
+    }
+
+    
+    if( confirm("Do you want to use lower case letters?")) {
+      choices = choices.concat(lowerCaseOptions)
+    }
+    
+    if( confirm("Do you want to use numbers?")) {
+      choices = choices.concat(numberOptions)
+    }
+    
+    if( confirm("Do you want to use special characters?")) {
+      choices = choices.concat(specialCharacterOptions)
+    }
+
+    if( choices.length === 0){
+      alert("Please select characters to use.")
+    }
+  }
+  return choices;
+}
 
 
 
 
 function generatePassword() {
     var length = userInputLength();
-    
+    var choices = userChoices();
+    var newPass = "";
+
+    for( i = 0 ; i < length; i++ )
+    {
+     newPass = newPass.concat(choices[ Math.floor(Math.random() * choices.length) ]);
+    }
+    return newPass;
+
 }
 
 // Write password to the #password input
